5c88b58e2a1f99854f422bc158a42e2c4c0d43ae
dave <dave@xxxx.(none)>
2014-05-05 21:56:33 -0400

    master, comments for gear functions

---
+++ gear-stl.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+# dave toal
+
+# function to cover a polygon with triangles
+# find the average point ?
+# use first three points and add from there ?
+
+# is right hand order clockwise or counterclockwise
+
+# surface normals, cross product function
+
+# get xspace files from dtoal.net for cross product
+
+# port xspace to javascript
+
+# put dtoal.net on github
+
+ 
